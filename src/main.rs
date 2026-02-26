86c4bbaeaf2fd47e5dda0001ee40e9725e77d682
rwiv <zc798546@gmail.com>
2026-02-27 07:06:11 +0900

    refactor: update logger config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use agb::app::{App, Cli};
 use clap::Parser;
+use env_logger::Env;
 
 fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let cli = Cli::parse();
     let app = App::new();
     app.run(cli)
